8b2432d05ef9a78d3d8816bbcd2a9292e6c37e03
markosbahgat111 <markos.bahgat111@gmail.com>
2022-04-27 00:45:02 +0200

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.scss';
+import drawer from './assets/images/drawers.jpg';
+import person from './assets/images/avatar-michelle.jpg';
+import share from './assets/images/icon-share.svg';
+import facebook from './assets/images/icon-facebook.svg';
+import twitter from './assets/images/icon-twitter.svg';
+import pintrest from './assets/images/icon-pinterest.svg';
 
-function App() {
+function App()
+{
+  const [show, setShow] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" onClick={() => show && setShow(false)}>
+      <div className='main_img_container'><img src={drawer} alt="drawer" /></div>
+      <div className='main_container'>
+        <h1>Shift the overall look and feel by adding these wonderful touches to furniture in our home.</h1>
+        <p>Ever been in a room and felt like something was missing? Perhaps it felt slightly bare and uninviting. i've got some simple tips to help you make any room feel complete.</p>
+        <div className='identity_container'>
+          <div className='person_img'>
+            <img src={person} alt="person" />
+          </div>
+          <p>
+            Michelle Appleton
+            <span>
+              28 Jun 2020
+            </span>
+          </p>
+          <div className='main_share_container'>
+              <div className='share_container' id={show ? "show" : undefined}>
+                <span>
+                    Share
+                </span>
+                <img src={facebook} alt="facebook"/>
+                <img src={twitter} alt="twitter"/>
+                <img src={pintrest} alt="pintrest"/>
+            </div>
+            <div className='share_icon' onClick={() => setShow(show => !show)} id={show ? "active" : undefined}>
+              <img src={share} alt="share Icon"/>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
